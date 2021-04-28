afd4fb975a1154c34006f7c071f525c0f5c572d3
DJ-JR30 <djtddy@hotmail.com>
2021-04-28 13:17:11 -0400

    First Pace

---
+++ index.js
@@ -0,0 +1,15 @@
+function NSFW(Type) {}
+
+module.exports.NSFW = NSFW;
+
+
+
+function PackageHelp() {
+  console.log("PackageHelp Function Called");
+  console.log("     ")
+  console.log("Functions:")
+  console.log("------------")
+  console.log("NSFW()")
+}
+
+module.exports.PackageHelpmodule.exports.Pack = PackageHelpPack;
